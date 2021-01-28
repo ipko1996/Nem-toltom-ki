7b611a8bcf603145fb6d88afcc8d98e6a6b49241
ipko1996 <ipko1996@gmail.com>
2021-01-29 00:42:44 +0100

    Update elte.js

--- elte.js
+++ elte.js
@@ -1,4 +1,4 @@
-//Ide jön a szegedi egyetem kódja
+//Ide jön az ELTE kódja
 //ELTE
 let button = document.querySelector(
   "#form1 > div.ui-dialog.ui-widget.ui-widget-content.ui-corner-all.ui-front.ui-draggable.ui-resizable > div.ui-dialog-titlebar.ui-widget-header.ui-corner-all.ui-helper-clearfix > div > button"
